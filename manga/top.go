c7ea243b7032c7df68f2d3b8cf186c75df0e2999
nokusukun <nokusukun@yahoo.co.jp>
2020-03-12 13:25:14 +0800

    fix(manga): implemented GetTop methods and structs

---
+++ manga/top.go
@@ -0,0 +1,72 @@
+package manga
+
+import (
+	"github.com/imroc/req"
+
+	"github.com/nokusukun/jikan2go/utils"
+)
+
+func GetTop(page int, subtype SubType) (Top, error) {
+	request, err := req.Get(utils.Contstants.AppendAPIf("/top/manga/%v/%v", page, subtype))
+	if err != nil {
+		return Top{}, err
+	}
+
+	var a Top
+	err = request.ToJSON(&a)
+
+	return a, err
+}
+
+type SubType string
+
+// manga novels oneshots doujin manhwa manhua
+const (
+	SubTypeManga      SubType = "manga"
+	SubTypeNovels     SubType = "novels"
+	SubTypeOneshots   SubType = "oneshots"
+	SubTypeDoujin     SubType = "doujin"
+	SubTypeManhwa     SubType = "manhwa"
+	SubTypeManhua     SubType = "manhua"
+	SubTypePopularity SubType = "bypopularity"
+	SubTypeFavourite  SubType = "favorite"
+)
+
+type Top struct {
+	RequestHash        string       `json:"request_hash"`
+	RequestCached      bool         `json:"request_cached"`
+	RequestCacheExpiry int64        `json:"request_cache_expiry"`
+	Top                []TopElement `json:"top"`
+}
+
+type TopElement struct {
+	MalID     int64   `json:"mal_id"`
+	Rank      int64   `json:"rank"`
+	Title     string  `json:"title"`
+	URL       string  `json:"url"`
+	Type      Type    `json:"type"`
+	Volumes   *int64  `json:"volumes"`
+	StartDate string  `json:"start_date"`
+	EndDate   *string `json:"end_date"`
+	Members   int64   `json:"members"`
+	Score     float64 `json:"score"`
+	ImageURL  string  `json:"image_url"`
+}
+
+func (r TopElement) GetID() int64 {
+	return r.MalID
+}
+
+func (r TopElement) GetType() string {
+	return "manga"
+}
+
+// manga novels oneshots doujin manhwa manhua
+const (
+	TopManga    Type = "Manga"
+	TopManhwa   Type = "Manhwa"
+	TopNovel    Type = "Novel"
+	TopOneshots Type = "Oneshots"
+	TopDoujin   Type = "Doujin"
+	TopManhua   Type = "Manhua"
+)
